bee0b1494c037908918c216b70a111d16ba5b9a1
Michał <64927330+CenturionTheMan@users.noreply.github.com>
2024-10-13 15:31:41 +0200

    cuda done?? - i did not build it because I am on wrong pc ...

--- source/headers/OddEvenSort.h
+++ source/headers/OddEvenSort.h
@@ -1,10 +1,18 @@
 #pragma once
 #include <thread>
 #include <vector>
+#include <cuda_runtime.h>
+#include "device_launch_parameters.h"
 
 namespace MultiThreaded
 {   
+    void OddEvenSortCuda(std::vector<int> &arr);
     void OddEvenSort(std::vector<int> &arr);
 }
 
 
+namespace CudaMethods
+{
+    __global__ void Odd(int* arr, int length);
+    __global__ void Even(int* arr, int length);
+} // namespace CudaMethods
--- source/implementations/OddEvenSort.cpp
+++ source/implementations/OddEvenSort.cpp
@@ -1,5 +1,44 @@
 #include "./../headers/OddEvenSort.h"
 
+void MultiThreaded::OddEvenSortCuda(std::vector<int> &arr)
+{
+    int  half = arr.size() / 2;
+    int *d_arr;
+    cudaMalloc(&d_arr, arr.size() * sizeof(int));
+    cudaMemcpy(d_arr, arr.data(), arr.size() * sizeof(int), cudaMemcpyHostToDevice);
+    for (int i = 0; i < half; i++)
+    {
+        CudaMethods::Even<<<1, half>>>(d_arr, arr.size());
+        CudaMethods::Odd<<<1, half>>>(d_arr, arr.size());
+        cudaDeviceSynchronize(); //?? must this be here?
+    }
+    cudaMemcpy(arr.data(), d_arr, arr.size() * sizeof(int), cudaMemcpyDeviceToHost);
+
+    cudaFree(d_arr);
+}
+
+__global__ void CudaMethods::Even(int *arr, int length) {
+    int index = threadIdx.x * 2;
+    if(index >= length) return;
+
+    if (arr[index] > arr[index + 1]) {
+        int tmp = arr[index];
+        arr[index] = arr[index+ 1];
+        arr[index + 1] = tmp;
+    }
+}
+
+__global__ void CudaMethods::Odd(int* arr, int length) {
+    int index = threadIdx.x * 2 + 1;
+    if(index >= length) return;
+
+    if (arr[index] > arr[index + 1]) {
+        int tmp = arr[index];
+        arr[index] = arr[index + 1];
+        arr[index + 1] = tmp;
+    }
+}
+
 
 void MultiThreaded::OddEvenSort(std::vector<int> &arr)
 {

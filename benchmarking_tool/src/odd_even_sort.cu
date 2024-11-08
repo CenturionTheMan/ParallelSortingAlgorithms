d1f10461da8e6c4443f02c7959a3b7332bc9c9a7
Xololt <105434979+xololt-dev@users.noreply.github.com>
2024-11-08 20:46:17 +0100

    Tryna fix memory usage

--- benchmarking_tool/src/odd_even_sort.cu
+++ benchmarking_tool/src/odd_even_sort.cu
@@ -1,6 +1,8 @@
 #include "odd_even_sort.cuh"
 #include <chrono>
+#include <cmath>
 #include <functional>
+#include <iostream>
 #include <thread>
 #include <vector>
 
@@ -96,7 +98,7 @@ void sorting::oldSort(std::vector<int>& arr) {
     }
 }
 
-void compare(std::vector<int>& arr, int startPoint, int endPoint) {
+static inline void compare(std::vector<int>& arr, const int startPoint, const int endPoint) {
     for (int i = startPoint; i <= endPoint; i += 2) {
         if (arr[i] > arr[i + 1]) {
             std::swap(arr[i], arr[i + 1]);
@@ -110,9 +112,86 @@ struct vecPoints {
 };
 
 void sorting::newSortJoin(std::vector<int>& arr) {
+    // std::cout << "Array size: " << (sizeof(std::vector<int>) + arr.size() * sizeof(int)) / 1024 << "kB\n";
+
     // Create threads
-    int splits = 10;
-    int sleepTime = 10;
+    const int arrSize = arr.size();
+    int localThreads = 1;
+    while (localThreads * 2 < std::thread::hardware_concurrency()) {
+        localThreads *= 2;
+    }
+    const int evenComparisons = arrSize / 2;
+    const int oddComparisons = (arrSize - 1) / 2;
+    const int splits = std::min(localThreads, std::max(1, int (evenComparisons / 1024)));
+    sorting::ThreadPool p(splits);
+    const int jobs[2] = {std::min(splits, evenComparisons), std::min(splits, oddComparisons)};
+    // const int jobs[2] = {evenComparisons, oddComparisons};
+    std::vector<vecPoints> evenPoints;
+    std::vector<vecPoints> oddPoints;
+    oddPoints.reserve(jobs[0]);
+    evenPoints.reserve(jobs[1]);
+    bool sorted = false;
+    bool odd = false;
+
+    // std::chrono::time_point<std::chrono::high_resolution_clock> now = std::chrono::high_resolution_clock::now();
+{
+    const int evenStep = std::max(2 * evenComparisons / splits, 2);
+    const int oddStep = std::max(2 * oddComparisons / splits, 2);
+
+    for (int oddInt = 0; oddInt < 2; oddInt++) {
+        int step = oddInt ? oddStep : evenStep;
+        int startPoint = int(oddInt);
+        int endPoint = step == 2 ? 1 + int(oddInt) : step - 2 * int(!oddInt);
+
+        for (int i = 0; i < jobs[oddInt]; i++) {
+            oddInt ? oddPoints.push_back({startPoint, endPoint}) : evenPoints.push_back({startPoint, endPoint});
+
+            startPoint = ++endPoint;
+            endPoint += step == 2 ? 1 : (oddInt ? step - 1 : step - 2);
+            if (i + 2 == splits) {
+                while (arrSize - endPoint > 2) {
+                    endPoint += 2;
+                }
+            }
+        }
+    }
+}
+
+    int place = 0;
+    // std:: cout << " Jobs: " << jobs[0] << " " << jobs[1] << std::endl;
+    while (!sorted) {
+        // Assign them 'splits' jobs
+        for (int j = 0; j < 2; j++) {
+            std::vector<vecPoints>& currentPoints = odd ? oddPoints : evenPoints;
+            for (int i = 0; i < jobs[odd]; i++) {
+                if (currentPoints[i].end < place) continue;
+                p.doJob(std::bind (compare, std::ref(arr), currentPoints[i].start, currentPoints[i].end));
+            }
+
+            odd = !odd;
+            while (!p.queueEmpty()) {
+                continue;
+            }
+        }
+        
+        // std::cout << "Wait: " << (std::chrono::high_resolution_clock::now() - now).count() << " ";
+        // now = std::chrono::high_resolution_clock::now();
+        sorted = true;
+        for (int i = 0; i < arrSize - 1; i++) {
+            if (arr[i] > arr[i+1]) {
+                sorted = false;
+                place = i - 1;
+                break;
+            }
+        }
+        // std::cout << "End: " << (std::chrono::high_resolution_clock::now() - now).count() << "\n";
+    }
+    // std::cout << std::endl;
+}
+
+void sorting::overSortJoin(std::vector<int>& arr) {
+    // Create threads
+    int splits = arr.size() % 10 ? std::min(16, int(arr.size() / 2)) : std::min(10, int(arr.size() / 2));
     int arrSize = arr.size();
     sorting::ThreadPool p (splits);
     std::vector<vecPoints> evenPoints;
@@ -148,37 +227,35 @@ void sorting::newSortJoin(std::vector<int>& arr) {
 }
 
     vecPoints range = {0, 0};
-    int loops = 0;
-    int jobs = std::min(splits, odd ? oddComparisons : evenComparisons);
+    int jobs[2] = {std::min(splits, evenComparisons), std::min(splits, oddComparisons)};
+    // int jobs = std::min(splits, odd ? oddComparisons : evenComparisons);
     while (!sorted) {
         sorted = true;
 
+        // std::chrono::time_point<std::chrono::high_resolution_clock> now = std::chrono::high_resolution_clock::now();
         // Assign them 'splits' jobs
-        jobs = std::min(splits, odd ? oddComparisons : evenComparisons);
-        for (int i = 0; i < jobs; i++) {
+        for (int i = 0; i < jobs[odd]; i++) {
             range = odd ? oddPoints[i] : evenPoints[i];
             p.doJob(std::bind (compare, std::ref(arr), range.start, range.end));
         }
+        // std::cout << "Job alloc: " << (std::chrono::high_resolution_clock::now() - now).count() << " ";
+        // now = std::chrono::high_resolution_clock::now();
 
-        odd = !odd;
-        loops = 0;
         while (!p.queueRefill()) {
-            std::this_thread::sleep_for(std::chrono::microseconds(sleepTime));
-            loops++;
         }
+        odd = !odd;
+        // std::cout << "Wait: " << (std::chrono::high_resolution_clock::now() - now).count() << " ";
+        // now = std::chrono::high_resolution_clock::now();
 
-        if (loops > 2) {
-            sleepTime++;
-        }
-        else if (loops == 0) {
-            sleepTime = std::max(sleepTime - 1, 1);
-        }
-
-        for (int i = 0; i < arrSize - 1; i++) {
-            if (arr[i] > arr[i+1]) {
-                sorted = false;
-                break;
+        if (sorted) {
+            for (int i = 0; i < arrSize - 1; i++) {
+                if (arr[i] > arr[i+1]) {
+                    sorted = false;
+                    break;
+                }
             }
         }
+        // std::cout << "End: " << (std::chrono::high_resolution_clock::now() - now).count() << "\n";
     }
+    // std::cout << std::endl;
 }
